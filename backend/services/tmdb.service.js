a2117c103a62bf4b43e53369e70aac2a0e41f643
cristianromano <63170214+cristianromano@users.noreply.github.com>
2025-01-11 23:59:24 -0300

    update

--- backend/services/tmdb.service.js
+++ backend/services/tmdb.service.js
@@ -1,4 +1,18 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config();
+
 export const fetchFromMovieDB = async (url) => {
-  const response = await fetch(url);
-  return response.json();
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization: `Bearer ${process.env.TMDB_ACCESS_TOKEN}`,
+    },
+  };
+
+  const res = await axios.get(url, options);
+
+  return res.data;
 };
