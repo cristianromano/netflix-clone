8bcb685920ea99fedad5f0dcab6b1ef3938331ba
cristianromano <63170214+cristianromano@users.noreply.github.com>
2025-01-19 04:37:38 -0300

    update backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,10 +23,10 @@ app.use("/api/v1/tv", tvShowsRouter);
 app.use("/api/v1/search", searchRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    res.sendFile(path.join(__dirname, "../frontend", "/dist", "index.html"));
   });
 }
 
